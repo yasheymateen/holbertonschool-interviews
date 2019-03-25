a3948229389055ed09947430cb4fd5cc1b2e44a4
Jinji Zhang <362@holbertonschool.com>
2019-03-25 21:10:16 +0000

    Remove comment

--- 0x01-warm_up/search_algos.h
+++ 0x01-warm_up/search_algos.h
@@ -5,7 +5,5 @@
 
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t size);
-//int binary_tree_is_full(const binary_tree_t *tree);
-
 
 #endif /*_SEARCH_ALGOS_H_ */
